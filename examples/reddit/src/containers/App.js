4d3e83e0522f80138997e48df7a3483ab93e0a73
Vlad Zhukov <me@vladzhukov.ru>
2018-11-21 14:07:53 +0300

    Update examples

--- examples/reddit/src/containers/App.js
+++ examples/reddit/src/containers/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Priem, Container} from 'priem';
+import {usePriem, Container} from 'priem';
 import Picker from '../components/Picker';
 import Posts from '../components/Posts';
 
-const reddit = new Container({
-    mapPropsToArgs: props => [props.redditName],
+const redditContainer = new Container({
     promise: reddit => {
         return fetch(`https://www.reddit.com/r/${reddit}.json`)
             .then(res => res.json())
@@ -14,36 +13,22 @@ const reddit = new Container({
     maxAge: 10000,
 });
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+export default () => {
+    const [redditName, setReddit] = React.useState('reactjs');
 
-        this.state = {reddit: 'reactjs'};
+    const reddit = usePriem(redditContainer, [redditName]);
 
-        this.handleChange = nextReddit => {
-            this.setState({reddit: nextReddit});
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <Picker value={this.state.reddit} onChange={this.handleChange} options={['reactjs', 'frontend']} />
-                <Priem redditName={this.state.reddit} sources={{reddit}}>
-                    {({reddit}, {refresh, pending}) => (
-                        <>
-                            <p>{reddit && <button onClick={refresh}>Refresh</button>}</p>
-                            {reddit ? (
-                                <div style={{opacity: pending ? 0.5 : 1}}>
-                                    <Posts posts={reddit} />
-                                </div>
-                            ) : (
-                                <h2>Loading...</h2>
-                            )}
-                        </>
-                    )}
-                </Priem>
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            <Picker value={redditName} onChange={setReddit} options={['reactjs', 'frontend']} />
+            <p>{reddit.data && <button onClick={reddit.refresh}>Refresh</button>}</p>
+            {reddit.data ? (
+                <div style={{opacity: reddit.pending ? 0.5 : 1}}>
+                    <Posts posts={reddit.data} />
+                </div>
+            ) : (
+                <h2>Loading...</h2>
+            )}
+        </div>
+    );
+};
