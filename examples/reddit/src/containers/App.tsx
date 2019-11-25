de7c129b6c7d0cf2626117e43c5ac7dbf833f79c
Vlad Zhukov <me@vladzhukov.ru>
2019-11-25 14:15:01 +0300

    Update examples

--- examples/reddit/src/containers/App.tsx
+++ examples/reddit/src/containers/App.tsx
@@ -3,28 +3,27 @@ import {createResource} from 'priem';
 import Picker from '../components/Picker';
 import Posts, {Post} from '../components/Posts';
 
-const useRedditContainer = createResource<Post[], {reddit: string}>(
-    ({reddit}) =>
-        fetch(`https://www.reddit.com/r/${reddit}.json`)
-            .then(res => res.json())
-            .then(res => res.data.children),
-    {
-        maxSize: 2,
-        maxAge: 10000,
-    },
+const useRedditContainer = createResource<Post[], {reddit: string; count: number}>(({reddit, count}) =>
+    fetch(`https://www.reddit.com/r/${reddit}.json?limit=10&count=${count}`)
+        .then(res => res.json())
+        .then(res => res.data.children),
 );
 
 export default () => {
     const [redditName, setReddit] = React.useState('reactjs');
-    const [reddit, {pending, refresh}] = useRedditContainer({reddit: redditName});
+    const [reddit, meta] = useRedditContainer.pages(
+        prevArgs => ({reddit: redditName, count: prevArgs ? prevArgs.count + 10 : 10}),
+        {refreshInterval: 10000},
+    );
 
     return (
         <div>
             <Picker value={redditName} onChange={setReddit} options={['reactjs', 'frontend']} />
-            <p>{reddit && <button onClick={refresh}>Refresh</button>}</p>
+            <p>{reddit && <button onClick={meta.invalidate}>Refresh</button>}</p>
             {reddit ? (
-                <div style={{opacity: pending ? 0.5 : 1}}>
-                    <Posts posts={reddit} />
+                <div style={{opacity: meta.pending ? 0.5 : 1}}>
+                    <Posts posts={reddit.reduce((acc, val) => acc.concat(val), [])} />
+                    <button onClick={meta.loadMore}>Load more</button>
                 </div>
             ) : (
                 <h2>Loading...</h2>
